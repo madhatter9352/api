9609e7f873470790e6942cdd08cd86c0088c00a5
lazaro <lachy9352@gmail.com>
2020-04-28 09:25:49 -0400

    Adjusting the out of the ProductResource

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Routing\Route;
 
 class ProductResource extends JsonResource
 {
@@ -18,8 +19,14 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'stock'=>$this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'discount'=>$this->discount,
+            'TotalPrice'=> round((1- $this->discount/100) * $this->price,2),
+            'rating'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2)
+                : 'No rating',
+            'href'=>[
+                'reviews'=>Route('reviews.index',$this->id)
+            ]
         ];
     }
 }
