3ebfab6aaba0f87e3a1b34edacf9cf9a468a2463
lazaro <lachy9352@gmail.com>
2020-04-28 08:59:01 -0400

    Creating ProductResource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
 use App\Product;
 use Illuminate\Http\Request;
 
@@ -15,6 +16,7 @@ class ProductController extends Controller
     public function index()
     {
         //
+        return Product::all();
     }
 
     /**
@@ -37,6 +39,7 @@ class ProductController extends Controller
     public function show(Product $product)
     {
         //
+        return new ProductResource($product);
     }
 
     /**
